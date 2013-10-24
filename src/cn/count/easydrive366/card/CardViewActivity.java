25e2d40521312a7925a659b1e66e0cfb04bd9032
fuxinyu@gmail.com <fuxinyu@gmail.com>
2013-10-24 18:29:31 +0800

    added card view detail

--- src/cn/count/easydrive366/card/CardViewActivity.java
+++ src/cn/count/easydrive366/card/CardViewActivity.java
@@ -3,6 +3,7 @@ package cn.count.easydrive366.card;
 import java.util.Map;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.ProgressDialog;
@@ -23,8 +24,8 @@ import cn.count.easydriver366.base.Menus;
 
 public class CardViewActivity extends BaseListViewActivity {
 
-private ProgressDialog _dialog;
-	
+	private ProgressDialog _dialog;
+	private JSONArray jsonList;
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -49,12 +50,14 @@ private ProgressDialog _dialog;
 		try{
 			//this.setupCompanyAndPhone(result);
 			result = ((JSONObject)result).getJSONObject("result");
-			JSONArray obj = ((JSONObject)result).getJSONArray("data");		
+			jsonList = ((JSONObject)result).getJSONArray("data");	
+			/*
 			for(int i=0;i<obj.length();i++){
 				JSONObject item = obj.getJSONObject(i);
 				
 			}
-			this.initList(obj);
+			*/
+			this.initList(jsonList);
 			
 		}catch(Exception e){
 			e.printStackTrace();
@@ -65,11 +68,14 @@ private ProgressDialog _dialog;
 	
 	@Override
 	protected void onListItemClick(final View view,final long index){
-		
-		if (_list!=null){
+		try {
+			JSONObject obj = jsonList.getJSONObject((int) index);
+			Intent intent = new Intent(this,CardDetailActivity.class);
+			intent.putExtra("data", obj.toString());
+			startActivity(intent);
+		} catch (JSONException e) {
 			
-			
-			
+			e.printStackTrace();
 		}
 	}
 	@Override
